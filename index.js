9d7814d07fbd0632ab896e096aa6a93e72fe5419
ColtonTO <colton1105@gmail.com>
2023-01-24 09:24:16 -0800

    added index.html, made basic inquirer prompts for index.js

--- index.js
+++ index.js
@@ -8,30 +8,112 @@ const Engineer = require("./lib/engineer")
 const Intern = require("./lib/intern")
 const Manager = require("./lib/manager")
 
-const questions = async () => {
-    const answers = await inquirer 
-    .prompt([
-        {
-            type: "input",
-            message: "What is your name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is your ID number?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is your email?",
-            name: "email",
-        },
+const questions = () => {
+    inquirer.prompt([
         {
             type: "list",
-            message: "Please pick a role",
+            message: "Please pick a role for your employee",
             name: "role",
             choices: ["Engineer", "Intern", "Manager"],
         },
-    ])
+    ]).then(
+        (answers) => {
+            if (answers.role === 'Intern') {
+                genIntern()
+            } else if (answers.role === 'Engineer') {
+                genEngineer()
+            } else{
+                genManager()
+            }
+        }
+    )
 }
 
+const genIntern = async () => {
+
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Name of person?',
+    },
+    {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID number?',
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    },
+    {
+    type: 'input',
+    name: 'school',
+    message: 'What is the name of your School?',
+    },
+])
+const {name,id,email,school} = answers
+const newIntern = new Intern(name,id,email,school)
+team.push(newIntern)
+
+}
+
+const genEngineer = async () => {
+
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Name of person?',
+    },
+    {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID number?',
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub?',
+    },
+])
+const {name,id,email,github} = answers
+const newEngineer = new Engineer(name,id,email,github)
+team.push(newEngineer)
+
+}
+
+const genManager = async () => {
+
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Name of person?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your office number?',
+    },
+])
+const {name,id,email,office}=answers
+const newManager = new Manager(name,id,email,office)
+team.push(newManager)
+
+}
+
+questions()
