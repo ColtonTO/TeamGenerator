e857ae30b2f243c9155c4634dff31ae0221a9fa5
ColtonTO <colton1105@gmail.com>
2023-01-25 14:20:22 -0800

    added tests for engineer, intern, manager. All passing

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,26 @@
+const Engineer = require('../lib/engineer')
+
+test('gets name', () => {
+    const engineer = new Engineer('John', 1, 'JohnDoe@gmail.com', 'githubusername01')
+
+    expect(engineer.getName()).toEqual(expect.any(String))
+});
+
+test('gets ID', () => {
+    const engineer = new Engineer('John', 3, 'JohnDoe@gmail.com', 'githubusername01');
+
+    expect(engineer.getId()).toEqual(expect.any(Number));
+});
+
+test('gets email', () => {
+    const engineer = new Engineer('John', 4, 'JohnDoe@gmail.com', 'githubusername01');
+
+    expect(engineer.getEmail()).toEqual(expect.stringContaining(engineer.email.toString()));
+});
+
+test('gets GitHub', () => {
+    const engineer = new Engineer('John', 4, 'JohnDoe@gmail.com', 'githubusername01');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
